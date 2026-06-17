74c77668ee3c5f12ff450a0febe9ce2f558be08c
Hunter S <github@mail.redkey.sh>
2026-06-16 22:15:14 -0700

    Drop quilt support: unable to support quilt 1.21.x + no 26.x releases yet

--- quilt/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-repositories {
-    maven("https://maven.quiltmc.org/repository/release/")
-}
-
-dependencies {
-    api(project(":core"))
-    implementation(project(":config"))
-    compileOnly("org.quiltmc:quilt-loader:0.29.1")
-}
