21d62058291f09489e26986ed20fa42310144096
david <mail@nonswag.dev>
2026-07-26 09:58:47 +0200

    Use latest hytale release

--- hytale/build.gradle.kts
+++ hytale/build.gradle.kts
@@ -13,11 +13,11 @@ tasks.compileJava {
 }
 
 repositories {
-    maven("https://maven.hytale.com/pre-release")
+    maven("https://maven.hytale.com/release")
 }
 
 dependencies {
     api(project(":core"))
     implementation(project(":config"))
-    compileOnly("com.hypixel.hytale:Server:2026.05.07-5efa15f6d")
+    compileOnly("com.hypixel.hytale:Server:+")
 }
