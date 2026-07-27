165b499fc15828eb19b6572dd6d894064ef94fd4
david <mail@nonswag.dev>
2026-07-27 20:55:32 +0200

    _proper?_ scaling

--- fabric/example-mod/src/main/java/com/example/mixin/OnboardingScreen.java
+++ fabric/example-mod/src/main/java/com/example/mixin/OnboardingScreen.java
@@ -27,6 +27,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @NullMarked
 @Mixin(TitleScreen.class)
@@ -40,7 +41,7 @@ public abstract class OnboardingScreen {
             Minecraft.getInstance().setScreenAndShow(new Onboarding());
         }
     }
-    
+
     // todo: add a button to open the onboarding screen somewhere in the settings
 
     private static final class Onboarding extends Screen {
@@ -59,6 +60,23 @@ public abstract class OnboardingScreen {
         private static final Component submitMetrics = Component.literal("Submit Metrics");
         private static final Component submitAdditionalMetrics = Component.literal("Submit Additional Metrics (provided by the developer)");
         private static final Component errorTracking = Component.literal("Submit Errors");
+
+        private static final int REFERENCE_WIDTH = 1280;
+        private static final int REFERENCE_HEIGHT = 720;
+        private static final int MAX_PANEL_WIDTH = 1186;
+        private static final int PANEL_MARGIN = 27;
+        private static final int PANEL_SIDE_MARGIN = 16;
+        private static final int TITLE_OFFSET = 45;
+        private static final int SCROLL_TOP_OFFSET = 88;
+        private static final int CONTENT_TOP_OFFSET = 122;
+        private static final int BOX_PADDING_X = 24;
+        private static final int BOX_PADDING_Y = 16;
+        private static final int BOX_GAP = 12;
+        private static final int CHECKBOX_GAP = 30;
+        private static final int CHECKBOX_TOP_GAP = 22;
+        private static final int BUTTON_TOP_GAP = 16;
+        private static final int BUTTON_GAP = 2;
+        private static final int BUTTON_HEIGHT = 20;
 
         private static final String[] alwaysCollectedData = new String[]{
                 "Mod version",
@@ -80,8 +98,6 @@ public abstract class OnboardingScreen {
         private boolean collectedDataExpanded;
         private boolean installedModsExpanded;
         private boolean submitMetricsSelected = true;
-        private boolean submitAdditionalMetricsSelected = true;
-        private boolean errorTrackingSelected = true;
         private int scrollOffset;
 
         private Checkbox submitMetricsWidget;
@@ -96,6 +112,9 @@ public abstract class OnboardingScreen {
 
         @Override
         protected void init() {
+            var submitAdditionalMetricsSelected = new AtomicBoolean(true);
+            var errorTrackingSelected = new AtomicBoolean(true);
+
             this.scrollOffset = Math.min(this.scrollOffset, this.maxScrollOffset());
 
             this.submitMetricsWidget = this.addRenderableWidget(Checkbox.builder(Onboarding.submitMetrics, this.font)
@@ -106,29 +125,30 @@ public abstract class OnboardingScreen {
                     })
                     .selected(this.submitMetricsSelected)
                     .build());
+
             this.submitAdditionalMetricsWidget = this.addRenderableWidget(Checkbox.builder(this.additionalMetricsLabel(), this.font)
-                    .pos(contentLeft(), checkboxY() + CHECKBOX_GAP)
-                    .onValueChange((checkbox, selected) -> this.submitAdditionalMetricsSelected = selected)
-                    .selected(this.submitAdditionalMetricsSelected && this.submitMetricsSelected)
+                    .pos(contentLeft(), checkboxY() + scaled(CHECKBOX_GAP))
+                    .onValueChange((checkbox, selected) -> submitAdditionalMetricsSelected.set(selected))
+                    .selected(submitAdditionalMetricsSelected.get() && this.submitMetricsSelected)
                     .build());
             this.submitAdditionalMetricsWidget.active = this.submitMetricsSelected;
             this.submitAdditionalMetricsWidget.setAlpha(this.submitMetricsSelected ? 1.0F : 0.45F);
             this.errorTrackingWidget = this.addRenderableWidget(Checkbox.builder(Onboarding.errorTracking, this.font)
-                    .pos(contentLeft(), checkboxY() + CHECKBOX_GAP * 2)
-                    .onValueChange((checkbox, selected) -> this.errorTrackingSelected = selected)
-                    .selected(this.errorTrackingSelected)
+                    .pos(contentLeft(), checkboxY() + scaled(CHECKBOX_GAP) * 2)
+                    .onValueChange((checkbox, selected) -> errorTrackingSelected.set(selected))
+                    .selected(errorTrackingSelected.get())
                     .build());
 
-            final var buttonGap = 2;
+            final var buttonGap = scaled(BUTTON_GAP);
             final var buttonWidth = (contentWidth() - buttonGap) / 2;
             this.declineButton = this.addRenderableWidget(Button.builder(Component.literal("Decline All").withColor(TextColor.RED), button -> {
                 // todo: disable all… close immediately or let the user also click "confirm"?
                 this.onClose();
-            }).bounds(contentLeft(), buttonY(), buttonWidth, 20).build());
+            }).bounds(contentLeft(), buttonY(), buttonWidth, buttonHeight()).build());
             this.acceptButton = this.addRenderableWidget(Button.builder(Component.literal("Confirm Selection").withColor(TextColor.GREEN), button -> {
                 // todo: save selection
                 this.onClose();
-            }).bounds(contentLeft() + buttonWidth + buttonGap, buttonY(), buttonWidth, 20).build());
+            }).bounds(contentLeft() + buttonWidth + buttonGap, buttonY(), buttonWidth, buttonHeight()).build());
             this.updateWidgetPositions();
         }
 
@@ -143,15 +163,15 @@ public abstract class OnboardingScreen {
         public void extractRenderState(final GuiGraphicsExtractor graphics, final int mouseX, final int mouseY, final float a) {
             super.extractRenderState(graphics, mouseX, mouseY, a);
 
-            final int panelWidth = Math.min(this.width - 32, 1186);
-            final int panelHeight = this.height - 54;
-            final int panelX = (this.width - panelWidth) / 2;
-            final int panelY = 27;
+            final int panelWidth = panelWidth();
+            final int panelHeight = panelHeight();
+            final int panelX = panelX();
+            final int panelY = panelY();
             final int border = 0xFFFFFFFF;
             final int text = 0xFFE0E0E0;
 
             graphics.outline(panelX, panelY, panelWidth, panelHeight, border);
-            graphics.centeredText(this.font, title, this.width / 2, panelY + 45, text);
+            graphics.centeredText(this.font, title, this.width / 2, panelY + scaled(TITLE_OFFSET), text);
 
             final int contentInset = contentInset(panelWidth);
             final int descriptionX = panelX + contentInset;
@@ -161,13 +181,13 @@ public abstract class OnboardingScreen {
             graphics.enableScissor(panelX + 1, scrollTop(), panelX + panelWidth - 1, scrollBottom());
             graphics.fill(descriptionX + 1, descriptionY + 1, descriptionX + descriptionWidth - 1, descriptionY + descriptionHeight - 1, 0xFF000000);
             graphics.outline(descriptionX, descriptionY, descriptionWidth, descriptionHeight, border);
-            graphics.textWithWordWrap(this.font, description, descriptionX + 24, descriptionY + 16, descriptionWidth - 48, text, false);
+            graphics.textWithWordWrap(this.font, description, descriptionX + scaled(BOX_PADDING_X), descriptionY + scaled(BOX_PADDING_Y), descriptionTextWidth(), text, false);
 
-            final int infoY = descriptionY + descriptionHeight + 12;
+            final int infoY = descriptionY + descriptionHeight + scaled(BOX_GAP);
             final int infoHeight = infoHeight();
             graphics.fill(descriptionX + 1, infoY + 1, descriptionX + descriptionWidth - 1, infoY + infoHeight - 1, 0xFF000000);
             graphics.outline(descriptionX, infoY, descriptionWidth, infoHeight, border);
-            graphics.textWithWordWrap(this.font, infoDescription(), descriptionX + 24, infoY + 16, descriptionWidth - 48, text, false);
+            graphics.textWithWordWrap(this.font, infoDescription(), descriptionX + scaled(BOX_PADDING_X), infoY + scaled(BOX_PADDING_Y), descriptionTextWidth(), text, false);
             graphics.disableScissor();
             this.renderScrollbar(graphics, panelX, panelY, panelWidth, panelHeight, border);
         }
@@ -269,14 +289,14 @@ public abstract class OnboardingScreen {
         }
 
         private int descriptionTextX() {
-            final int panelWidth = Math.min(this.width - 32, 1186);
-            final int panelX = (this.width - panelWidth) / 2;
+            final int panelWidth = panelWidth();
+            final int panelX = panelX();
             final int contentInset = contentInset(panelWidth);
-            return panelX + contentInset + 24;
+            return panelX + contentInset + scaled(BOX_PADDING_X);
         }
 
         private int descriptionTextY() {
-            return descriptionY() + 16;
+            return descriptionY() + scaled(BOX_PADDING_Y);
         }
 
         private int infoTextX() {
@@ -284,31 +304,28 @@ public abstract class OnboardingScreen {
         }
 
         private int infoTextY() {
-            return infoY() + 16;
+            return infoY() + scaled(BOX_PADDING_Y);
         }
 
         private int contentLeft() {
-            final int panelWidth = Math.min(this.width - 32, 1186);
-            final int panelX = (this.width - panelWidth) / 2;
+            final int panelWidth = panelWidth();
+            final int panelX = panelX();
             final int contentInset = contentInset(panelWidth);
             return panelX + contentInset + 2;
         }
 
         private int contentWidth() {
-            final int panelWidth = Math.min(this.width - 32, 1186);
+            final int panelWidth = panelWidth();
             final int contentInset = contentInset(panelWidth);
             return panelWidth - contentInset * 2 - 4;
         }
 
-        // fixme: magic numbers here we go!!! all of this is bound to break :)
-        private static final int CHECKBOX_GAP = 30;
-        
         private int descriptionY() {
-            return 27 + 122 - this.scrollOffset;
+            return contentTop() - this.scrollOffset;
         }
 
         private int baseInfoY() {
-            return 27 + 122 + descriptionHeight() + 12 - this.scrollOffset;
+            return contentTop() + descriptionHeight() + scaled(BOX_GAP) - this.scrollOffset;
         }
 
         private int infoY() {
@@ -316,23 +333,24 @@ public abstract class OnboardingScreen {
         }
 
         private int scrollTop() {
-            return 27 + 88;
+            return panelY() + scaled(SCROLL_TOP_OFFSET);
         }
 
         private int scrollBottom() {
-            return this.height - 55;
+            return panelY() + panelHeight() - 1;
         }
 
         private int checkboxY() {
-            return this.infoY() + infoHeight() + 22;
+            return this.infoY() + infoHeight() + scaled(CHECKBOX_TOP_GAP);
         }
-        
+
         private int buttonY() {
-            return checkboxY() + CHECKBOX_GAP * 2 + 20 + 16;
+            return checkboxY() + scaled(CHECKBOX_GAP) * 2 + buttonHeight() + scaled(BUTTON_TOP_GAP);
         }
 
         private int contentBottom() {
-            return 27 + 122 + descriptionHeight() + 12 + infoHeight() + 22 + CHECKBOX_GAP * 2 + 20 + 16 + 20;
+            return contentTop() + descriptionHeight() + scaled(BOX_GAP) + infoHeight() + scaled(CHECKBOX_TOP_GAP)
+                    + scaled(CHECKBOX_GAP) * 2 + buttonHeight() + scaled(BUTTON_TOP_GAP) + buttonHeight();
         }
 
         private int maxScrollOffset() {
@@ -342,22 +360,22 @@ public abstract class OnboardingScreen {
         private void updateWidgetPositions() {
             this.scrollOffset = Math.min(this.scrollOffset, this.maxScrollOffset());
 
-            final int buttonGap = 2;
+            final int buttonGap = scaled(BUTTON_GAP);
             final int buttonWidth = (contentWidth() - buttonGap) / 2;
             this.submitMetricsWidget.setY(checkboxY());
-            this.submitAdditionalMetricsWidget.setY(checkboxY() + CHECKBOX_GAP);
-            this.errorTrackingWidget.setY(checkboxY() + CHECKBOX_GAP * 2);
+            this.submitAdditionalMetricsWidget.setY(checkboxY() + scaled(CHECKBOX_GAP));
+            this.errorTrackingWidget.setY(checkboxY() + scaled(CHECKBOX_GAP) * 2);
             this.submitMetricsWidget.visible = isInsideScrollArea(this.submitMetricsWidget.getY(), this.submitMetricsWidget.getHeight());
             this.submitAdditionalMetricsWidget.visible = isInsideScrollArea(this.submitAdditionalMetricsWidget.getY(), this.submitAdditionalMetricsWidget.getHeight());
             this.errorTrackingWidget.visible = isInsideScrollArea(this.errorTrackingWidget.getY(), this.errorTrackingWidget.getHeight());
             this.declineButton.setX(contentLeft());
             this.declineButton.setY(buttonY());
             this.declineButton.setWidth(buttonWidth);
-            this.declineButton.setHeight(20);
+            this.declineButton.setHeight(buttonHeight());
             this.acceptButton.setX(contentLeft() + buttonWidth + buttonGap);
             this.acceptButton.setY(buttonY());
             this.acceptButton.setWidth(buttonWidth);
-            this.acceptButton.setHeight(20);
+            this.acceptButton.setHeight(buttonHeight());
             this.declineButton.visible = isInsideScrollArea(this.declineButton.getY(), this.declineButton.getHeight());
             this.acceptButton.visible = isInsideScrollArea(this.acceptButton.getY(), this.acceptButton.getHeight());
         }
@@ -392,18 +410,18 @@ public abstract class OnboardingScreen {
         }
 
         private int descriptionTextWidth() {
-            final int panelWidth = Math.min(this.width - 32, 1186);
+            final int panelWidth = panelWidth();
             final int contentInset = contentInset(panelWidth);
             final int descriptionWidth = panelWidth - contentInset * 2;
-            return descriptionWidth - 48;
+            return Math.max(1, descriptionWidth - scaled(BOX_PADDING_X) * 2);
         }
 
         private int descriptionHeight() {
-            return Math.max(76, this.height / 5);
+            return Math.max(scaled(76), this.height / 5);
         }
 
         private int infoHeight() {
-            return Math.max(60, this.font.wordWrapHeight(infoDescription(), descriptionTextWidth()) + 32);
+            return Math.max(scaled(60), this.font.wordWrapHeight(infoDescription(), descriptionTextWidth()) + scaled(BOX_PADDING_Y) * 2);
         }
 
         // todo: maybe an object that keeps track of that? collapsed text and everything…
@@ -441,8 +459,48 @@ public abstract class OnboardingScreen {
             return component;
         }
 
-        private static int contentInset(final int panelWidth) {
-            return Math.clamp(panelWidth / 8, 24, 160);
+        private int panelX() {
+            return (this.width - panelWidth()) / 2;
+        }
+
+        private int panelY() {
+            return scaled(PANEL_MARGIN);
+        }
+
+        private int panelWidth() {
+            final int margin = scaled(PANEL_SIDE_MARGIN);
+            final int availableWidth = Math.max(1, this.width - margin * 2);
+            return Math.min(availableWidth, scaled(MAX_PANEL_WIDTH));
+        }
+
+        private int panelHeight() {
+            return Math.max(1, this.height - panelY() * 2);
+        }
+
+        private int contentTop() {
+            return panelY() + scaled(CONTENT_TOP_OFFSET);
+        }
+
+        private int buttonHeight() {
+            return Math.max(BUTTON_HEIGHT, scaled(BUTTON_HEIGHT));
+        }
+
+        private int scaled(final int value) {
+            return Math.max(1, Math.round(value * this.layoutScale()));
+        }
+
+        private float layoutScale() {
+            if (this.width <= 0 || this.height <= 0) {
+                return 1.0F;
+            }
+
+            final float widthScale = this.width / (float) REFERENCE_WIDTH;
+            final float heightScale = this.height / (float) REFERENCE_HEIGHT;
+            return Math.clamp(Math.min(widthScale, heightScale), 0.65F, 1.25F);
+        }
+
+        private int contentInset(final int panelWidth) {
+            return Math.clamp(panelWidth / 8, scaled(24), scaled(160));
         }
 
         private static Component collapsible(final String text, final boolean expanded) {
